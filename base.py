98dd8a3c779b55f9931e9b8fff8ec0d21dfb69a4
moawezz <moawez.de@gmail.com>
2024-02-07 00:52:07 +0330

    Create BaseClass

---
+++ base.py
@@ -0,0 +1,19 @@
+from abc import ABC
+
+
+class BaseClass(ABC):
+    _id = 0
+    objects_list = list()
+
+    def __init__(self):
+        self.id = self.generate_id()
+        self.store(self)
+
+    @classmethod
+    def generate_id(cls):
+        cls._id += 1
+        return cls._id
+
+    @classmethod
+    def store(cls, obj):
+        cls.objects_list.append(obj)
