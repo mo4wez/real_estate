0e786a3d05dd66edbd90e04c81c730e996be57cc
moawezz <moawez.de@gmail.com>
2024-02-08 00:23:14 +0330

    add super to user init

--- user.py
+++ user.py
@@ -2,10 +2,10 @@ from base import BaseClass
 
 class User(BaseClass):
     def __init__(self, first_name: str, last_name: str, phone_number: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
+        super().__init__(*args, **kwargs)
 
     @property
     def fullname(self):
