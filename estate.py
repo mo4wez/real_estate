0d98c3aaf882b210492b60e159d17a4bc398fd6a
moawezz <moawez.de@gmail.com>
2024-02-08 00:23:49 +0330

    create Estates Entities

---
+++ estate.py
@@ -0,0 +1,44 @@
+from abc import abstractmethod
+from base import BaseClass
+
+
+class EstateAbstract(BaseClass):
+    def __init__(self, user, area: float, rooms_count: int, built_year: int, region, address: str, *args, **kwargs):
+        self.user = user
+        self.area = area
+        self.rooms_count = rooms_count
+        self.built_year = built_year
+        self.region = region
+        self.address = address
+        super().__init__(*args, **kwargs)
+
+    @abstractmethod
+    def show_description(self):
+        pass
+        
+
+class Apartment(EstateAbstract):
+    def __init__(self, has_elevator: bool, has_parking: bool, floor: int, *args, **kwargs):
+        self.has_elevator = has_elevator
+        self.has_parking = has_parking
+        self.floor = floor
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f'Apartment {self.id}\t area: {self.area}')
+
+
+class House(EstateAbstract):
+    def __init__(self, has_yard: bool, floors_count: int, *args, **kwargs):
+        self.has_yard = has_yard
+        self.floors_count = floors_count
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f'House {self.id}')
+
+
+class Shop(EstateAbstract):
+    
+    def show_description(self):
+        print(f'Shop {self.id}')
