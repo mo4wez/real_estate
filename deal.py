d626efdbe751e2b32b4ed234cc30aa00bb0eb3b8
moawezz <moawez.de@gmail.com>
2024-02-08 00:24:38 +0330

    create deal - sell, rent

---
+++ deal.py
@@ -0,0 +1,22 @@
+from base import BaseClass
+
+
+class Sell(BaseClass):
+    def __init__(self, price_per_meter: float, discountable: bool, convertible: bool, *args, **kwargs):
+        self.price_per_meter = price_per_meter
+        self.discountable = discountable
+        self.convertible = convertible
+        super().__init__(*args, **kwargs)
+
+    def show_price(self):
+        print(f'Price: {self.price_per_meter}\t Discount: {self.discountable}\t Convrtible: {self.convertible}')
+
+
+class Rent(BaseClass):
+    def __init__(self, initial_price: float, monthly_price: float, discountable: bool, convertible: bool, *args, **kwargs):
+        self.initial_price = initial_price
+        self.monthly_price = monthly_price
+        self.discountable = discountable
+        self.convertible = convertible
+        super().__init__(*args, **kwargs)
+    
